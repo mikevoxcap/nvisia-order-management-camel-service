10a22ddd20ce334ed5e6669f60b90003587aa66b
mikevoxcap <mike@michaelhoffmaninc.com>
2016-07-07 19:26:55 -0500

    Updates

--- src/main/java/com/nvisia/examples/camel/ordermanagement/OrderManagementRouter.java
+++ src/main/java/com/nvisia/examples/camel/ordermanagement/OrderManagementRouter.java
@@ -22,65 +22,83 @@ public class OrderManagementRouter extends FatJarRouter {
 
    @Override
    public void configure() {
+      initializeRestConfiguration();
+      initializeOrderManagementRoute();
+   }
+
+   protected void initializeRestConfiguration() {
       // Start by building an instance of RestConfigurationDefinition. Need to
       // specify the component we are going to use for enabling REST endpoints,
       // specifically CamelServlet in this case. Set the binding mode to JSON.
-      restConfiguration().
+      restConfiguration()
             // Leverage the CamelServlet component for the REST DSL
-            component("servlet").
+            .component("servlet")
             // Bind using JSON
-            bindingMode(RestBindingMode.json).
+            .bindingMode(RestBindingMode.json)
             // I like pretty things...
-            dataFormatProperty("prettyPrint", "true").
+            .dataFormatProperty("prettyPrint", "true")
             // This is the context path to be used for Swagger API documentation
-            apiContextPath("api-doc").
+            .apiContextPath("api-doc")
             // Properties for Swagger
             // Title of the API
-      apiProperty("api.title", "Order Management API").
+            .apiProperty("api.title", "Order Management API")
             // Version of the API
-            apiProperty("api.version", "1.0.0").
+            .apiProperty("api.version", "1.0.0")
             // CORS (resource sharing) enablement
-            apiProperty("cors", "true").
+            .apiProperty("cors", "true")
             // Use localhost for calls
-            apiProperty("host", "localhost:8082").
+            .apiProperty("host", "localhost:8082")
             // Set base path
-            apiProperty("base.path", "nvisia-order-management-camel-service/api");
+            .apiProperty("base.path", "nvisia-order-management-camel-service/api");
+   }
 
+   protected void initializeOrderManagementRoute() {
       // Definition of the post order endpoint
-      rest("/order").
+      rest("/order")
             // This is a POST method call for submitting an order using the
             // order form
-      post().
+            .post()
             // Description of what the method does
-            description("Submit a new order").
+            .description("Submit a new order")
             // Define the type used for input
-            type(OrderForm.class).
+            .type(OrderForm.class)
             // Define the type used for output
-            outType(Order.class).
+            .outType(Order.class)
             // Route direct to process order by type
-            to("direct:routeOrderByType");
+            .to("direct:routeOrderByType");
 
       // Definition of the content based routing endpoint
-      from("direct:routeOrderByType").
+      from("direct:routeOrderByType")
             // Use the Content Based Router EIP to process the order based on
             // the item type
-      choice().
+            .choice()
+            // Check for Catalog Item as book
+            .when()
             // When this is a book, send it to the process book method
-            when()
             .simple("${body.catalogItem.catalogItemType} == '"
                   + CatalogItemType.BOOK.toString() + "'")
-            .to("bean:orderManagementService?method=processBookOrder(${body})").
+            // Send it to the method for book processing
+            .to("bean:orderManagementService?method=processBookOrder(${body})")
+            // Check for Catalog Item as clothing
+            .when()
             // When this is clothing, send it to the process clothing method
-            when()
             .simple("${body.catalogItem.catalogItemType} == '"
                   + CatalogItemType.CLOTHING.toString() + "'")
-            .to("bean:orderManagementService?method=processClothingOrder(${body})").
+            // Send it to the method for clothing processing
+            .to("bean:orderManagementService?method=processClothingOrder(${body})")
+            // Check for Catalog Item as electronics
+            .when()
             // When this is electronics, send it to the process electronics
             // method
-      when().simple("${body.catalogItem.catalogItemType} == '"
-            + CatalogItemType.ELECTRONICS.toString() + "'")
+            .simple("${body.catalogItem.catalogItemType} == '"
+                  + CatalogItemType.ELECTRONICS.toString() + "'")
+            // Send it to the method for electronics processing
             .to("bean:orderManagementService?method=processElectronicsOrder(${body})")
-            .otherwise().throwException(new Exception("Invalid catalog item type"));
+            // Catch other items
+            .otherwise()
+            // Throw an exception when the catalog item type is not supported
+            // for processing.
+            .throwException(new Exception("Invalid catalog item type"));
    }
 
    @Bean
